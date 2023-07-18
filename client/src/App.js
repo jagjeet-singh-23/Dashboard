f689f94c980fbceeca8b166e7a09ecdcb372c784
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-07-18 19:07:42 +0530

    modified:   client/src/App.js 	new file:   client/src/scenes/dashboard/index.jsx 	new file:   client/src/scenes/layout/index.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
+import Dashboard from "scenes/dashboard";
+import Layout from "scenes/layout";
 
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
@@ -11,9 +14,20 @@ const App = () => {
   }, [mode]);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route
+                path="/"
+                element={<Navigate to={`/dashboard`} replace />}
+              />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 };
