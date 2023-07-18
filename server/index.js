f0568a5e9e388abef4cb85aea8144da9e40b43ed
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-07-18 13:23:08 +0530

    new file:   client/.gitignore 	new file:   client/README.md 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   client/public/favicon.ico 	new file:   client/public/index.html 	new file:   client/public/logo192.png 	new file:   client/public/logo512.png 	new file:   client/public/manifest.json 	new file:   client/public/robots.txt 	new file:   client/src/App.css 	new file:   client/src/App.js 	new file:   client/src/App.test.js 	new file:   client/src/index.css 	new file:   client/src/index.js 	new file:   client/src/logo.svg 	new file:   client/src/reportWebVitals.js 	new file:   client/src/setupTests.js 	new file:   server/.gitignore 	new file:   server/controllers/client.js 	new file:   server/controllers/general.js 	new file:   server/controllers/management.js 	new file:   server/controllers/sales.js 	new file:   server/index.js 	new file:   server/package-lock.json 	new file:   server/package.json 	new file:   server/routes/client.js 	new file:   server/routes/general.js 	new file:   server/routes/management.js 	new file:   server/routes/sales.js

---
+++ server/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+// CONFIGURATION
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+// MONGOOSE SETUP
+
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server listening on port:${PORT}`))
+  )
+  .catch(err => console.log(`${err} did not connect`));
