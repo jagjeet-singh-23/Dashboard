78d70513fd44fe7389036cb836832ed6aa075183
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-07-18 18:44:12 +0530

    modified:   client/src/App.js 	modified:   client/src/index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
 
 const App = () => {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => {
+    createTheme(themeSettings(mode));
+  }, [mode]);
   return (
-    <div className='app'>App</div>
-  )
-}
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
+};
 
-export default App
+export default App;
