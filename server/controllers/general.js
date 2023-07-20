982d200502c8745c42402f09dcd5801cc4a32afb
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-07-20 18:07:37 +0530

    modified:   client/src/scenes/layout/index.jsx 	modified:   server/controllers/general.js 	modified:   server/routes/general.js

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -4,8 +4,8 @@ export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
-    res.status(200).json(user);
+    return res.status(200).json(user);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    return res.status(404).json({ message: error.message });
   }
 };
