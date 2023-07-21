97776032a188399c95af8e5305465ec0f68a8ac1
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-07-21 16:29:50 +0530

    modified:   client/src/components/Navbar.jsx 	modified:   client/src/components/Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   Box,
   Divider,
@@ -28,6 +28,7 @@ import {
   TrendingUpOutlined,
   PieChartOutlined,
 } from "@mui/icons-material";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
 import placeholder from "assets/placeholder.jpg";
@@ -92,10 +93,11 @@ const navItems = [
 ];
 
 const Sidebar = ({
+  user,
   drawerWidth,
-  isNonMobile,
   isSidebarOpen,
   setIsSidebarOpen,
+  isNonMobile,
 }) => {
   const { pathname } = useLocation();
   const [active, setActive] = useState("");
@@ -105,8 +107,9 @@ const Sidebar = ({
   useEffect(() => {
     setActive(pathname.substring(1));
   }, [pathname]);
+
   return (
-    <Box component={`nav`}>
+    <Box component="nav">
       {isSidebarOpen && (
         <Drawer
           open={isSidebarOpen}
@@ -118,18 +121,18 @@ const Sidebar = ({
             "& .MuiDrawer-paper": {
               color: theme.palette.secondary[200],
               backgroundColor: theme.palette.background.alt,
-              boxSizing: "border-box",
+              boxSixing: "border-box",
               borderWidth: isNonMobile ? 0 : "2px",
               width: drawerWidth,
             },
           }}
         >
-          <Box width={"100%"}>
-            <Box m="1.5rem 2rem 2rem 3rem">
+          <Box width="100%">
+            <Box m="1.5rem 2rem 1.5rem 3rem">
               <FlexBetween color={theme.palette.secondary.main}>
-                <Box display={"flex"} alignItems={"center"} gap={"0.5rem"}>
-                  <Typography variant="h4" fontWeight={"bold"}>
-                    TMHOBSDK
+                <Box display="flex" alignItems="center" gap="0.5rem">
+                  <Typography variant="h4" fontWeight="bold">
+                    ECOMVISION
                   </Typography>
                 </Box>
                 {!isNonMobile && (
@@ -143,7 +146,7 @@ const Sidebar = ({
               {navItems.map(({ text, icon }) => {
                 if (!icon) {
                   return (
-                    <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
+                    <Typography key={text} sx={{ m: "1.5rem 0 0 3rem" }}>
                       {text}
                     </Typography>
                   );
@@ -188,6 +191,42 @@ const Sidebar = ({
                 );
               })}
             </List>
+          </Box>
+
+          <Box position="absolute" bottom="1rem">
+            <Divider />
+            <FlexBetween textTransform="none" gap="1rem" m="1rem 2rem 0 3rem">
+              <Box
+                component="img"
+                alt="profile"
+                src={placeholder}
+                height="40px"
+                width="40px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.9rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.8rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <SettingsOutlined
+                sx={{
+                  color: theme.palette.secondary[300],
+                  fontSize: "25px ",
+                }}
+              />
+            </FlexBetween>
           </Box>
         </Drawer>
       )}
