08a2509a6f89445ca9af2978855d8bc008390097
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-07-21 16:31:05 +0530

    modified:   client/src/scenes/layout/index.jsx

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -10,8 +10,7 @@ const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const userId = useSelector((state) => state.global.userId);
-  const { data } = useGetUserQuery(userId);
-  console.log("data:", data );
+  const { data } = useGetUserQuery(userId);  
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
